054c84d8ab7673cbe43e61a48f188779f5ef7506
John Hobson <3026384+jhob101@users.noreply.github.com>
2024-06-04 08:56:42 +0100

    Set version as pre-release
    Took 22 seconds

--- trufi-maps.php
+++ trufi-maps.php
@@ -4,13 +4,19 @@
  * Plugin Name: Trufi Route Pages
  * Plugin URI: https://trufi.app/
  * Description: A plugin for displaying Trufi Maps.
- * Version: 1.0
+ * Version: 0.7.0
  * Author: Trufi Association
  * Author URI: https://trufi.app/
  * License: GPL2
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: trufi-maps
  */
+
+if( ! function_exists('get_plugin_data') ){
+    require_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+}
+$plugin_data = get_plugin_data( __FILE__, false );
+define( 'TRUFI_ROUTES_PLUGIN_VERSION', ($plugin_data && $plugin_data['Version']) ? $plugin_data['Version'] : '1.0.0' );
 
 require_once('scripts/constants.php');
 require_once('scripts/functions.php');
