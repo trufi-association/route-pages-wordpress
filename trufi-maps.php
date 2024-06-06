70c00a498d9f9555057d7861c19c40678985154f
John Hobson <3026384+jhob101@users.noreply.github.com>
2024-06-06 11:16:31 +0100

    Add PHP version check
    Took 9 minutes

--- trufi-maps.php
+++ trufi-maps.php
@@ -4,7 +4,7 @@
  * Plugin Name: Trufi Route Pages
  * Plugin URI: https://trufi.app/
  * Description: A plugin for displaying Trufi Maps.
- * Version: 0.8
+ * Version: 0.8.1
  * Author: Trufi Association
  * Author URI: https://trufi.app/
  * License: GPL2
@@ -18,16 +18,29 @@ if (!function_exists('get_plugin_data')) {
 $plugin_data = get_plugin_data(__FILE__, false);
 define('TRUFI_ROUTES_PLUGIN_VERSION', ($plugin_data && $plugin_data['Version']) ? $plugin_data['Version'] : '1.0.0');
 
+// check php version if less than 8.0 don't load
+if (version_compare(phpversion(), '8.4', '<')) {
+    add_action('admin_notices', 'trufi_php_version_notice');
+    function trufi_php_version_notice()
+    {
+        ?>
+        <div class="notice notice-error is-dismissible">
+            <p><?php _e('Trufi Route Pages plugin requires PHP 8.0 or higher. Please update your PHP version.', 'TrufiApi-maps'); ?></p>
+        </div>
+        <?php
+    }
+    return;
+}
+
 require('functions/constants.php');
 require('api/TrufiApi.php');
-
-//if (is_admin()) {
-    require('admin/transient-cache-clear.php');
-    require('admin/admin-settings.php');
-//}
-
 require('functions/functions.php');
 require('functions/enqueue-scripts.php');
 require('functions/rewrite-rules.php');
 require('functions/template-redirect.php');
 require('functions/sitemap-provider.php');
+
+if (is_admin()) {
+    require('admin/transient-cache-clear.php');
+    require('admin/admin-settings.php');
+}
